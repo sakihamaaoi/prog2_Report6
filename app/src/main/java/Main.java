8d46866ace9dd1ea132beca02419435baa9cf99e
Aoi Sakihama <e235723@ie.u-ryukyu.ac.jp>
2024-01-23 23:34:01 +0900

    Main commit

---
+++ app/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import jp.ac.uryukyu.ie.e235723.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ボードのサイズは7です ");
+        System.out.println("地雷の数は10です ");
+        var minesweeper = new MineSweeper();
+        minesweeper.gameStart();
+           
+    }
+ 
+}
+
+
